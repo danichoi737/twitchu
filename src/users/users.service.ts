f4abeed89ff29def2d65c7f856d75608c166a12f
wookookie <wookookie404@gmail.com>
2025-01-30 12:06:40 +0900

    feat: Enhance user creation with validation and transaction management

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,9 +1,8 @@
 import { pbkdf2, randomBytes } from "node:crypto";
 import { promisify } from "node:util";
-import { Injectable } from "@nestjs/common";
+import { BadRequestException, ConflictException, Injectable } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
-import { InjectRepository } from "@nestjs/typeorm";
-import { Repository } from "typeorm";
+import { DataSource } from "typeorm";
 import { User } from "./entities/user.entity";
 import { CreateUserDto } from "./dto/create-user.dto";
 
@@ -11,29 +10,52 @@ import { CreateUserDto } from "./dto/create-user.dto";
 export class UsersService {
   constructor(
     private readonly configService: ConfigService,
-    @InjectRepository(User) private readonly usersRepository: Repository<User>,
+    private readonly dataSource: DataSource,
   ) {}
 
   public async create(createUserDto: CreateUserDto) {
     const { email, password } = createUserDto;
 
-    // TO-DO: 상세 구현
+    // falsy 값 확인
+    if (!email || !password) {
+      throw new BadRequestException("Invalid email or password");
+    }
+
+    const queryRunner = this.dataSource.createQueryRunner();
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+    // 중복 확인
+    const user = await queryRunner.manager.getRepository(User).findOne({ where: { email } });
+    if (user) {
+      throw new ConflictException("Already exist user");
+    }
 
     const keylen = 64;
     const salt = randomBytes(keylen);
     const iterations = parseInt(this.configService.get("CRYPTO_ITERATIONS"));
     const pbkdf2Async = promisify(pbkdf2);
     try {
+      // 비밀번호 암호화
       const derivedKey = await pbkdf2Async(password, salt, iterations, keylen, "sha512");
 
+      // 사용자 생성
       const user = new User();
       user.email = email;
       user.password = derivedKey.toString("hex");
       user.salt = salt.toString("hex");
+      const returned = await queryRunner.manager.getRepository(User).save(user);
+      await queryRunner.commitTransaction();
 
-      return this.usersRepository.save(user);
+      // 신규 사용자 이메일 반환
+      return returned.email;
     } catch (error) {
+      await queryRunner.rollbackTransaction();
+
+      // TO-DO: 암호화 오류 구분 필요
+      console.error(error);
       throw error;
+    } finally {
+      await queryRunner.release();
     }
   }
 }
